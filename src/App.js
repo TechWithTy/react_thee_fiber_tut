219062f4c8df69ed592b1d591e7659480ba10b90
Tyrique Daniel <37724661+TyriqueDaniel@users.noreply.github.com>
2021-06-27 02:04:22 -0600

    AutoCommit

--- src/App.js
+++ src/App.js
@@ -1,39 +1,64 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef } from 'react';
 //R3F
 import { Canvas, useFrame } from 'react-three-fiber';
-// Deai - R3F
-// import { softShadows, MeshWobbleMaterial, OrbitControls } from '@react-three/drei';
-//Components
-// import Header from './components/header';
-// Styles
+import { softShadows,MeshWobbleMaterial } from '@react-three/drei';
 import './App.scss';
 // React Spring
-// import { useSpring, a } from '@react-spring/three';
 
-// soft Shadows
-// softShadows();
-const SpinningMesh = ({pos,args}) => {
+softShadows()
+const SpinningMesh = ({ pos, args, color }) => {
   const mesh = useRef(null);
-  useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01))
+  useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01));
   return (
-    <mesh position={pos} ref={mesh}>
+    <mesh castShadow position={pos} ref={mesh}>
       <boxBufferGeometry attach="geometry" args={args} />
-      <meshStandardMaterial attach="material" />
+      <MeshWobbleMaterial attach="material" color={color} speed={1} factor={0.6} />
     </mesh>
   );
-}
-
+};
 
 const App = () => {
   return (
     <>
       {/* <Header /> */}
       {/* Our Scene & Camera is already built into our canvas */}
-      <Canvas colorManagment camera={{ position: [-5, 2, 10], fov: 60 }}>
-        <ambientLight intensity={0.3} color="pink" />
-        <SpinningMesh pos={[0, 1, 0]} args={[3,2,1]}/>
-        <SpinningMesh pos={[-2, 1, -5]} />
-        <SpinningMesh pos={[5, 1, -2]} />
+      <Canvas
+        shadow
+        colorManagment
+        camera={{ position: [-5, 2, 10], fov: 60 }}
+      >
+        {/* //Light */}
+        <ambientLight intensity={0.3} />
+        <pointLight position={[-10, 0, -20]} intensity={0.5} />
+        <pointLight position={[0, -10, 0]} intensity={1.5} />
+        <directionalLight
+          castShadow
+          position={[0, 10, 0]}
+          intensity={1.5}
+          shadow-mapSize-width={1024}
+          shadow-mapSize-height={1024}
+          shadow-camera-far={50}
+          shadow-camera-left={-10}
+          shadow-camera-right={10}
+          shadow-camera-top={50}
+          shadow-camera-bottom={-10}
+        />
+
+        {/* //PLane */}
+        <group>
+          <mesh
+            rec
+            rotation={[-Math.PI / 2, 0, 0]}
+            position={[0, -3, 0]}
+          >
+            <planeBufferGeometry attach="geometry" args={[100, 100]} />
+            <shadowMaterial attach="material" opacity={0.3} />
+          </mesh>
+        </group>
+
+        <SpinningMesh pos={[0, 1, 0]} args={[3, 2, 1]} color="lightBlue" />
+        <SpinningMesh pos={[-2, 1, -5]} color="pink" />
+        <SpinningMesh pos={[5, 1, -2]} color="pink" />
       </Canvas>
     </>
   );
