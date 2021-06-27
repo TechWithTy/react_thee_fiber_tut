24f2ad11465d77d90fadc6aedb3e05689161eed9
Tyrique Daniel <37724661+TyriqueDaniel@users.noreply.github.com>
2021-06-27 01:14:29 -0600

    AutoCommit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
-import { Canvas } from 'react-three-fiber';
-import { meshStandardMaterial } from 'three';
-
+import React, { useRef, useState } from 'react';
+//R3F
+import { Canvas, useFrame } from 'react-three-fiber';
+// Deai - R3F
+// import { softShadows, MeshWobbleMaterial, OrbitControls } from '@react-three/drei';
+//Components
+// import Header from './components/header';
+// Styles
 import './App.scss';
-function App() {
+// React Spring
+// import { useSpring, a } from '@react-spring/three';
+
+// soft Shadows
+// softShadows();
+const Box = () => {
+  const mesh = useRef(null);
+  useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01))
+  return (
+    <mesh ref={mesh}>
+      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
+      <meshStandardMaterial attach="material"/>
+    </mesh>
+  )
+}
+
+
+const App = () => {
   return (
     <>
+      {/* <Header /> */}
+      {/* Our Scene & Camera is already built into our canvas */}
       <Canvas>
-        <mesh>
-          <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
-          <meshStandardMaterial attach="material" />
-        
-        </mesh>
+        <Box/>
       </Canvas>
     </>
   );
-}
+};
 
 export default App;
