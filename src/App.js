09f1d10d5666d66e96fa419d18d5e01709832cd0
Tyrique Daniel <37724661+TyriqueDaniel@users.noreply.github.com>
2021-06-27 02:25:13 -0600

    AutoCommit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
-import React, { useRef } from 'react';
-//R3F
+import { useSpring,a } from '@react-spring/three';
+import {
+  MeshWobbleMaterial,
+  OrbitControls,
+  softShadows,
+} from '@react-three/drei';
+import React, { useRef, useState } from 'react';
 import { Canvas, useFrame } from 'react-three-fiber';
-import { softShadows,MeshWobbleMaterial } from '@react-three/drei';
 import './App.scss';
+
 // React Spring
 
-softShadows()
-const SpinningMesh = ({ pos, args, color }) => {
+softShadows();
+const SpinningMesh = ({ pos, args, color, speed, factor }) => {
+  const [expand, setExpand] = useState(false);
   const mesh = useRef(null);
   useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01));
+  const props = useSpring({
+    scale: expand ? [1.4, 1.4, 1.4] : [1, 1, 1],
+  });
   return (
-    <mesh castShadow position={pos} ref={mesh}>
+    <a.mesh
+      onClick={() => setExpand(!expand)}
+      scale={props.scale}
+      castShadow
+      position={pos}
+      ref={mesh}
+    >
       <boxBufferGeometry attach="geometry" args={args} />
-      <MeshWobbleMaterial attach="material" color={color} speed={1} factor={0.6} />
-    </mesh>
+      <MeshWobbleMaterial
+        attach="material"
+        color={color}
+        speed={speed}
+        factor={factor}
+      />
+    </a.mesh>
   );
 };
 
@@ -22,11 +42,7 @@ const App = () => {
     <>
       {/* <Header /> */}
       {/* Our Scene & Camera is already built into our canvas */}
-      <Canvas
-        shadow
-        colorManagment
-        camera={{ position: [-5, 2, 10], fov: 60 }}
-      >
+      <Canvas shadow colorManagment camera={{ position: [-5, 2, 10], fov: 60 }}>
         {/* //Light */}
         <ambientLight intensity={0.3} />
         <pointLight position={[-10, 0, -20]} intensity={0.5} />
@@ -47,18 +63,24 @@ const App = () => {
         {/* //PLane */}
         <group>
           <mesh
-            rec
+            receiveShadow
             rotation={[-Math.PI / 2, 0, 0]}
             position={[0, -3, 0]}
           >
             <planeBufferGeometry attach="geometry" args={[100, 100]} />
             <shadowMaterial attach="material" opacity={0.3} />
           </mesh>
+          <SpinningMesh
+            pos={[0, 1, 0]}
+            args={[3, 2, 1]}
+            color="lightBlue"
+            speed={2}
+          />
+          <SpinningMesh pos={[-2, 1, -5]} color="pink" speed={6} />
+          <SpinningMesh pos={[5, 1, -2]} color="pink" speed={6} />
         </group>
 
-        <SpinningMesh pos={[0, 1, 0]} args={[3, 2, 1]} color="lightBlue" />
-        <SpinningMesh pos={[-2, 1, -5]} color="pink" />
-        <SpinningMesh pos={[5, 1, -2]} color="pink" />
+        <OrbitControls />
       </Canvas>
     </>
   );
